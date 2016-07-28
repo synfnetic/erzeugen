2f6bb5394f1a4538b57cbd7ae8028d23767a8001
Anthony D'Ambrosio <adambrosio@thenavisway.com>
2016-07-28 13:24:50 -0700

    Automated release commit for: 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject synfnetic/erzeugen "0.1.0-SNAPSHOT"
+(defproject synfnetic/erzeugen "0.1.0"
   :license {:name "MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.8.51"]
